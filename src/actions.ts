5e30eceae889488d495464f3fbb7940524819913
felipe <pipega2000@hotmail.com>
2022-07-31 11:32:51 -0500

    specialities async functions created

--- src/actions.ts
+++ src/actions.ts
@@ -1,25 +1,45 @@
+import { specialityI,patientI } from "../src/index";
 
 
+export async function getAllSpecialities() {
+  const response:Response = await fetch('http://localhost:8080/api/v1/specialities')
 
-export interface speciality{
+  const data:specialityI[] = await response.json()
 
-    name: string,
-    physician: string,
-    specialityId: number|null
+  return data
+} 
 
+export async function postSpeciality(speciality:specialityI){
+  const response:Response = await fetch('http://localhost:8080/api/v1/create/speciality', 
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' 
+    },
+    body: JSON.stringify(speciality)
+  })
+
+  return response;
 }
 
+export async function deleteSpeciality(id:string){
+  const response:Response = await fetch(`http://localhost:8080/api/v1/delete/speciality/${id}`, 
+  {
+    method: 'DELETE'
+  })
 
+  return response;
+}
 
-export interface patient{
+export async function putSpeciality(speciality:specialityI){
+  const response:Response = await fetch('http://localhost:8080/api/v1/update/speciality', 
+  {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json' 
+    },
+    body: JSON.stringify(speciality)
+  })
 
-    patientId: number|null,
-    name: string,
-    age: number,
-    dni: number,
-    speciality: {
-        specialityId: number
-    }
-
-
+  return response;
 }
